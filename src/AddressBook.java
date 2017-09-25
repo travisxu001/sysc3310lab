ffc53f63212c4e1fe64193590a87f98ff15bca50
shaoyixu <shaoyixu@cb5109-04.labs.sce.carleton.ca>
2017-09-25 09:35:09 -0400

    Add methods to the main function

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,25 @@
 
 public class AddressBook {
+	BuddyInfo buddyInfo = new BuddyInfo();
+	BuddyInfo T2 = new BuddyInfo();
+	BuddyInfo T3 = new BuddyInfo();
+	
+	public void addBuddy(BuddyInfo T) {
+		if(T != null) {
+			buddyInfo.add(T);
+		}
+	}
+	
+	public void removeBuddy(BuddyInfo T) {
+		buddyInfo.remove(T);
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Address Book");
+	}
+	
+	
+	
 
 }
